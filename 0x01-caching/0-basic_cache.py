5056c93028e3b6ffc7bd95f047d5c2f05803eed4
Young-proflat <olatunbosunlateef6@gmail.com>
2024-07-25 03:56:02 +0000

    Caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
-"""BasicCaching module"""
-
-from typing import Dict. Any. Optional
+"""Basic caching module.
+"""
 from base_caching import BaseCaching
 
+
 class BasicCache(BaseCaching):
-    """ child class """
-    def __init__(self):
-        """classs of the parent class"""
-        super().__init__()
-    def put(self, key: Any, item: Any) -> None:
-        """ put method by adding a cache data """
+    """Represents an object that allows storing and
+    retrieving items from a dictionary.
+    """
+    def put(self, key, item):
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
         self.cache_data[key] = item
 
-    def get(self, key: Any) -> Optional[Any]:
-        """ Get method for retrieving cache """
-        return self.cache_data.get(key)
+    def get(self, key):
+        """Retrieves an item by key.
+        """
+        return self.cache_data.get(key, None)
     
