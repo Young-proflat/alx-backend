83d2b7babad77dc3dae29cb288fadbea97e5dacc
Young-proflat <olatunbosunlateef6@gmail.com>
2024-07-31 02:40:17 +0000

    babel

---
+++ 0x02-il8n/1-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""A basic Flask app.
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """ represents a flask configuration
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@app.route('/')
+def get_index() -> str:
+    """home page
+    """
+    return render_template('1-index.html')
+
+
+if__name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
