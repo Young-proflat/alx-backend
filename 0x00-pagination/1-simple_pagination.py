6ca5bbfbe9cfd27434c307b60b18e5e8c79bae0f
Young-proflat <olatunbosunlateef6@gmail.com>
2024-08-15 17:23:40 +0000

    Pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-"""Paginate a csv"""
+"""Paginate a csv
+"""
 
 from typing import List, Tuple
+
 
 def index_range(page: int, page_size: int) -> Tuple[int: int]:
     """retrieves the page index start and end
@@ -10,17 +12,13 @@ def index_range(page: int, page_size: int) -> Tuple[int: int]:
     end = start + page_size
     return (start, end)
 
-
-
 def get_page(self, page: int = 1, page_size: int = 10) ->List[List]:
     """retireves page content 
     """
-    assert isinstance (page, int) and page > 0
-    assert isinstance (page_size, int) & page_size > 0
-
+    assert isinstance(page, int) and page > 0
+    assert isinstance(page_size, int) and page_size > 0
     start, end = index_range(page, page_size)
     data = self.dataset()
     if start > len(data):
         return []
     return data[start:end]
-
